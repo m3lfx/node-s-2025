c09982fb8e6acbb74ce8b01bb9d041eb75abc686
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-14 11:26:45 +0800

    add quantity

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 const items = require('./routes/item');
+
+app.use(cors())
 app.use('/api/v1', items);
 module.exports = app
