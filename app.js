5303d6dde343fd5a4882aad6a80c50665444193c
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-14 11:51:12 +0800

    static image files

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const path = require('path')
 
 const items = require('./routes/item');
 
 app.use(cors())
+app.use('/images', express.static(path.join(__dirname, 'images')))
+
+
 app.use('/api/v1', items);
 module.exports = app
