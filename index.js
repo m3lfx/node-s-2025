4e629dd811058084f19b3fd7094cf5d32aeb6bd7
rommel dalisay <rommelmdalisay@gmail.com>
2025-06-14 08:49:24 +0800

    app

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const app = require('./app');
+const app = require('./app')
 
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-
-app.listen(PORT, () => {
-    console.log(PORT)
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => {  
+  console.log(`Server is running on port ${PORT}`)
+})
